8c505887fbfc6906a347354f05a9a69e9105b8e2
Henrique Barbosa <henrikleod@gmail.com>
2022-02-02 22:58:29 -0300

    Creating Poll and PollOption seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+poll = Poll.create(finished_at: nil)
+
+poll.poll_options.create [
+    {name: "Java", photo: "http://assets.stickpng.com/images/58480979cef1014c0b5e4901.png" },
+    {name: "C#", photo: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"},
+    {name: "PHP", photo: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" }
+]
