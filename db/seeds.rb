91b9b7d3548c39d5d017ddf5c9c560664c0722f0
Henrique Barbosa <henrikleod@gmail.com>
2022-02-02 23:06:55 -0300

    Creating cool seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -10,7 +11,10 @@
 poll = Poll.create(finished_at: nil)
 
 poll.poll_options.create [
-    {name: "Java", photo: "http://assets.stickpng.com/images/58480979cef1014c0b5e4901.png" },
-    {name: "C#", photo: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"},
-    {name: "PHP", photo: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" }
+  { name: 'Java',
+    photo: 'http://assets.stickpng.com/images/58480979cef1014c0b5e4901.png' },
+  { name: 'C#',
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png' },
+  { name: 'PHP',
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg' }
 ]
