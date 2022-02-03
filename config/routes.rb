3ca16ac7238ce355f783e1d348723cbad5e5d072
Henrique Barbosa <henrikleod@gmail.com>
2022-02-03 10:57:27 -0300

    Add the ranking route (post because we are using Turbo Stream)

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'polls#index'
   post '/vote' => 'polls#vote', as: :vote
+  post '/ranking' => 'polls#ranking', as: :ranking
 end
